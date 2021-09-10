6892a72f78506838fa456294ea1babc082285013
Abu Tayeb Mohammada Ayon <mohammadayo@outlook.com>
2021-09-10 23:25:56 +0600

    Renamed

--- LinkedLIst/main.cpp
+++ LinkedLIst/main.cpp
@@ -3,61 +3,59 @@
 using namespace std;
 
 int main()
-{  
-    linked_list ll;
+{
+  linked_list ll;
 
-    int n,inFrst,inLst;
-    cout<<"Enter the number elements that to add at first :";
-    cin>>inFrst;
-    while (inFrst)
+  int n, inFrst, inLst;
+  cout << "Enter the number elements that to add at first :";
+  cin >> inFrst;
+  while (inFrst)
+  {
+    cin >> n;
+    if (n != 0)
     {
-        cin >> n;
-        if (n != 0)
-        {
-            ll.insertAtFirst(n);
-        }
-        else
-        {
-            break;
-        }
-        inFrst--;
+      ll.insertAtFirst(n);
     }
-    cout << "\nPrinted the list : " << endl;
-    ll.print();
-    cout << "\nEnter the number elements that to add at lasy :";
-    cin >> inLst;
-    while (inLst)
+    else
     {
-        cin >> n;
-        if (n != 0)
-        {
-            ll.insertAtLast(n);
-        }
-        else
-        {
-            break;
-        }
-        inLst--;
+      break;
     }
-    cout << "\nPrinted the list : " << endl;
-    ll.print();
-    cout << "\nInsert 46 at position 3" << endl;
-    ll.insertAtPosition(3, 25);
-    ll.print();
-    cout << "\nFirst node is deleted" << endl;
-    ll.deleteAtStart();
-    ll.print();
-    cout << "\nLast node is deleted" << endl;
-    ll.DeleteAtLast();
-    ll.print();
-    cout << "\nNode of position 2 is deleted" << endl;
-    ll.deleteAtPosition(2);
-    ll.DeleteValue(2);
-    ll.print();
-    ll.reverse();
-    ll.print();
-   
-    
-   
-    return 0;
+    inFrst--;
+  }
+  cout << "\nPrinted the list : " << endl;
+  ll.print();
+  cout << "\nEnter the number elements that to add at lasy :";
+  cin >> inLst;
+  while (inLst)
+  {
+    cin >> n;
+    if (n != 0)
+    {
+      ll.insertAtLast(n);
+    }
+    else
+    {
+      break;
+    }
+    inLst--;
+  }
+  cout << "\nPrinted the list : " << endl;
+  ll.print();
+  cout << "\nInsert 46 at position 3" << endl;
+  ll.insertAtPosition(3, 25);
+  ll.print();
+  cout << "\nFirst node is deleted" << endl;
+  ll.deleteAtStart();
+  ll.print();
+  cout << "\nLast node is deleted" << endl;
+  ll.DeleteAtLast();
+  ll.print();
+  cout << "\nNode of position 2 is deleted" << endl;
+  ll.deleteAtPosition(2);
+  ll.DeleteValue(2);
+  ll.print();
+  ll.reverse();
+  ll.print();
+
+  return 0;
 }
